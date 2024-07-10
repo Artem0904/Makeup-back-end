5aeccc3d5bd20b532007b2ef5c7ac887e95dc3a5
HP <>
2024-07-10 18:25:01 +0300

    Add necessary entities

---
+++ DataAccess/Data/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data.Entities
+{
+    public class User
+    {
+        public int UserTypeId { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
