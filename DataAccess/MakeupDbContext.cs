a98c1e2233a23298eee0f5f7b18b457d42f7dcec
HP <hp@DESKTOP-J2K3IOQ>
2024-07-10 20:15:13 +0300

    Add identity and configure db context

--- DataAccess/MakeupDbContext.cs
+++ DataAccess/MakeupDbContext.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using DataAccess.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class MakeupDbContext
+    public class MakeupDbContext : IdentityDbContext<User>
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Gender> Genders { get; set; }
 
+        public MakeupDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ DataAccess/SampleContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess
+{
+    public class SampleContextFactory : IDesignTimeDbContextFactory<MakeupDbContext>
+    {
+        public MakeupDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MakeupDbContext>();
+
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            IConfigurationRoot config = builder.Build();
+
+            string? connectionString = config.GetConnectionString("LocalDb");
+            optionsBuilder.UseSqlServer(connectionString);
+            return new MakeupDbContext(optionsBuilder.Options);
+        }
+    }
+}
