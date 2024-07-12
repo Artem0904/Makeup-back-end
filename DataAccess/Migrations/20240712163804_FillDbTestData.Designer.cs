cd22e56724d4c75d2048ca2e8ffd6c9ae81c3681
Artem0904 <moonportal708@gmail.com>
2024-07-12 19:39:23 +0300

    Fill db by test data

--- DataAccess/Migrations/20240712163804_FillDbTestData.Designer.cs
+++ DataAccess/Migrations/20240712163804_FillDbTestData.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(MakeupDbContext))]
-    partial class MakeupDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240712163804_FillDbTestData")]
+    partial class FillDbTestData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
